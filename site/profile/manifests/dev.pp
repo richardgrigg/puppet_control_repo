ada599d5c0f6acbee152f9189469443bd802cbf1
Richard Grigg <richardgrigg@gmail.com>
2020-06-10 16:23:59 +0100

    Update dev.pp

--- site/profile/manifests/dev.pp
+++ site/profile/manifests/dev.pp
@@ -1,1 +1,22 @@
-include wget
+class profile::dev {
+  include wget
+  
+  class { nano:
+    $default => false,
+    $tabsize => '4',
+  }
+  
+  include git
+  git::config { 'user.name':
+    value => 'richard.grigg',
+  }
+
+  git::config { 'user.email':
+    value => 'richardgrigg@gmail.com',
+  }
+  
+
+}
+
+
+
